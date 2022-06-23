864b7c7f3a50e3d1a0ccb5c07a38d7a688e9b301
alexrice236 <alexrice826@gmail.com>
2022-06-22 22:42:57 -0400

    servo script

---
+++ testScripts/servo.py
@@ -0,0 +1,16 @@
+import keyboard
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(13, GPIO.OUT)
+pwm = GPIO.PWM(13, 1000)
+
+while True:
+    if keyboard.read_key() == 'a':
+        pwm.start(70)
+    elif keyboard.read_key() == 'd':
+        pwm.start(30)
+    else:
+        pwm.stop()
