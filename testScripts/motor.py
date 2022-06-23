0419a0268da8922ddf82727d783f8596e753a6c3
alexrice236 <alexrice826@gmail.com>
2022-06-22 20:41:25 -0400

    user input

--- testScripts/motor.py
+++ testScripts/motor.py
@@ -1,3 +1,4 @@
+import keyboard
 import RPi.GPIO as GPIO
 import time
 
@@ -10,12 +11,12 @@ GPIO.output(6, GPIO.LOW)
 #GPIO.output(12, GPIO.HIGH)
 pwm = GPIO.PWM(12, 1000)
 
-for i in range(75):
-    pwm.start(i)
-    time.sleep(.5)
+#for i in range(75):
+#    pwm.start(i)
+#    time.sleep(.5)
 
-#while True:
-#    if keyboard.read_key() == "w":
-#        pwm.start(50)
-#    else:
-#        pwm.stop()
+while True:
+    if keyboard.read_key() == "w":
+        pwm.start(70)
+    else:
+        pwm.stop()
