908c67cf4e9a2c1e4223f9306a09edee8703059e
alexrice236 <alexrice826@gmail.com>
2022-06-22 23:34:05 -0400

    servo script

--- testScripts/servo.py
+++ testScripts/servo.py
@@ -5,13 +5,21 @@ from gpiozero import Servo
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(13, GPIO.OUT)
-servo = GPIO.PWM(13, 50)
-servo.start(7.5)
+servo = Servo(13)
 
-for i in range(2, 13):
-    servo.ChangeDutyCycle(i)
-    time.sleep(1)
+servo.min()
+time.sleep(1)
+servo.mid()
+time.sleep(1)
+servo.max()
 
-servo.stop()
-GPIO.cleanup()
+#GPIO.setup(13, GPIO.OUT)
+#servo = GPIO.PWM(13, 50)
+#servo.start(0)
+
+#for i in range(2, 13):
+#    servo.ChangeDutyCycle(i)
+#    time.sleep(1)
+
+#servo.stop()
+#GPIO.cleanup()
