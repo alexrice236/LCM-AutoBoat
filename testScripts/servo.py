1201cd5d33958ebec19e17760803312fb5c50456
alexrice236 <alexrice826@gmail.com>
2022-06-22 23:05:08 -0400

    servo script

--- testScripts/servo.py
+++ testScripts/servo.py
@@ -6,7 +6,8 @@ from gpiozero import Servo
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(13, GPIO.OUT)
-pwm = GPIO.PWM(13, 1000)
+pwm = GPIO.PWM(13, 50)
+pwm.start(0)
 
 for i in range(2, 13):
     pwm.ChangeDutyCycle(i)
