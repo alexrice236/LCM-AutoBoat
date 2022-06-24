f73661c184bb36bbba9a7e05e294a8b6fd161138
alexrice236 <alexrice826@gmail.com>
2022-06-23 20:55:51 -0400

    sshkeyboard added

--- testScripts/ui.py
+++ testScripts/ui.py
@@ -1,20 +1,24 @@
-import keyboard
+from sshkeyboard import listen_keyboard
+#import keyboard
 import lcm
 
 from modata import motion_data
 
 lc = lcm.LCM()
 
-while True:
+def press(key):
     msg = motion_data()
     msg.linear_speed = 0.0
     msg.angle = 0.0
 
-    if keyboard.read_key() == "w":
+    if key == "w":
         msg.linear_speed = 1
-    if keyboard.read_key() == "a":
+    if key == "a":
         msg.angle = -1
-    elif keyboard.read_key() == "d":
+    elif key == "d":
         msg.angle = 1
 
     lc.publish("MOTION", msg.encode())
+
+while True:
+    listen_keyboard(on_press=press)
