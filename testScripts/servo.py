4df459f6bb1d516ab12f5f780059482fd1963d27
alexrice236 <alexrice826@gmail.com>
2022-06-25 14:58:43 -0400

    servo troubleshooting

--- testScripts/servo.py
+++ testScripts/servo.py
@@ -10,9 +10,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(13, GPIO.OUT)
 servo = GPIO.PWM(13, 50)
 servo.start(0)
+old_angle = 0
 
 def my_handler(channel, data):
     msg = motion_data.decode(data)
+    if msg.angle == old_angle:
+        return
     servo.start(msg.angle)
     time.sleep(.1)
     servo.stop()
