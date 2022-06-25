1ac3e65934359f507c67d66b1002a862e749ad7e
alexrice236 <alexrice826@gmail.com>
2022-06-25 04:02:44 -0400

    servo

--- testScripts/servo.py
+++ testScripts/servo.py
@@ -4,13 +4,15 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(13, GPIO.OUT)
-servo = GPIO.PWM(13, 50)
+GPIO.setup(12, GPIO.OUT)
+servo = GPIO.PWM(12, 50)
 servo.start(0)
 
 for i in range(2, 13):
+    time.sleep(1)
     servo.ChangeDutyCycle(i)
     time.sleep(1)
+    servo.ChangeDutyCycle(0)
 
 servo.stop()
 GPIO.cleanup()
