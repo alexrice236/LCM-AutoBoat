a0de61c7ec0c69e46b0f172b8f90c9dceccfd43d
alexrice236 <alexrice826@gmail.com>
2022-06-25 14:43:48 -0400

    servo troubleshooting

--- testScripts/servo.py
+++ testScripts/servo.py
@@ -1,32 +1,18 @@
 import keyboard
 import RPi.GPIO as GPIO
 import time
-import lcm
-from modata import motion_data
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(13, GPIO.OUT)
 servo = GPIO.PWM(13, 50)
-
-
-def my_handler(channel, data):
-    msg = motion_data.decode(data)
-    servo.ChangeDutyCycle(10)
-    #servo.ChangeDutyCycle(msg.angle)
-
-lc = lcm.LCM()
-subscription = lc.subscribe("MOTION", my_handler)
-
 servo.start(0)
 
 time.sleep(1)
-servo.ChangeDutyCycle(10)
+
+servo.ChangeDutyCycle(7.5)
+
 time.sleep(1)
 
-while True:
-    servo.start(0)
-    lc.handle()
-    servo.stop()
-
+servo.stop()
 GPIO.cleanup()
