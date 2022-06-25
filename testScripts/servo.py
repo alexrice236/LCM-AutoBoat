a3b42dc3c7339eade5b69942c1d1f62a379621a0
alexrice236 <alexrice826@gmail.com>
2022-06-25 16:15:31 -0400

    revert

--- testScripts/servo.py
+++ testScripts/servo.py
@@ -9,26 +9,14 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(13, GPIO.OUT)
 servo = GPIO.PWM(13, 50)
-old_angle = 0
-
-def get_old_angle():
-    return old_angle
-
-def set_old_angle(angle):
-    global old_angle
-    old_angle = angle
 
 def my_handler(channel, data):
     msg = motion_data.decode(data)
-    print(get_old_angle())
-    #if msg.angle == get_old_angle():
-    #    return
     print("starting pwm")
     servo.start(0)
     servo.ChangeDutyCycle(msg.angle)
     time.sleep(.1)
     servo.stop()
-    set_old_angle(msg.angle)
 
 lc = lcm.LCM()
 subscription = lc.subscribe("MOTION", my_handler)
