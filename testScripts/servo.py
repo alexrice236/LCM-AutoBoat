c2bdf6d34fc08c8069cf37588eb80e934deef63b
alexrice236 <alexrice826@gmail.com>
2022-06-25 11:31:29 -0400

    changed pin for servo

--- testScripts/servo.py
+++ testScripts/servo.py
@@ -4,8 +4,8 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(12, GPIO.OUT)
-servo = GPIO.PWM(12, 50)
+GPIO.setup(13, GPIO.OUT)
+servo = GPIO.PWM(13, 50)
 servo.start(0)
 
 for i in range(2, 13):
