ce833a05b0a7f53d8bffc93aee0b9cbcb3f08118
alexrice236 <alexrice826@gmail.com>
2022-07-13 17:22:50 -0400

    second teleop iter

--- testScripts/ui.py
+++ testScripts/ui.py
@@ -12,7 +12,7 @@ def press(key):
     msg.angle = 0.0
 
     if key == "w":
-        msg.linear_speed = 50
+        msg.linear_speed = 76
     if key == "a":
         msg.angle = 10
     if key == "s":
