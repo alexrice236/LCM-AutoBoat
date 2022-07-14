b40c728d487064000eb942beddb64a3586f4466f
alexrice236 <alexrice826@gmail.com>
2022-07-14 05:54:55 -0400

    motor switch

--- testScripts/ui.py
+++ testScripts/ui.py
@@ -12,7 +12,7 @@ def press(key):
     msg.angle = 0.0
 
     if key == "w":
-        msg.linear_speed = 75
+        msg.linear_speed = 80
     if key == "a":
         msg.angle = 10
     if key == "s":
