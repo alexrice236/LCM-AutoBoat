469eecb6698eb82601135d9dd6dd8e91b9408e2f
alexrice236 <alexrice826@gmail.com>
2022-07-21 13:53:02 -0400

    power graph

--- testScripts/power_graph.py
+++ testScripts/power_graph.py
@@ -17,25 +17,22 @@ def my_handler(channel, data):
     current_y.append(msg.current)
     power_y.append(msg.power)
     time_x.append(time.time())
+    plt.subplot(3,1,1)
+    plt.plot(np.array(time_x), np.array(voltage_y))
+    plt.title("Voltage (V)")
+    plt.subplot(3,1,2)
+    plt.plot(np.array(time_x), np.array(current_y))
+    plt.title("Current (mA)")
+    plt.subplot(3,1,3)
+    plt.plot(np.array(time_x), np.array(power_y))
+    plt.title("Power (W)")
+    plt.xlabel("Time (s)")
+    plt.savefig("/home/pi/LCM-AutoBoat/testScripts/graph.png")
 
 lc = lcm.LCM()
 subscription = lc.subscribe("POWER", my_handler)
 
 while True:
-    try:
-        plt.savefig("/home/pi/LCM-AutoBoat/testScripts/graph.png")
-        lc.handle()
-    except KeyboardInterrupt:
-        plt.subplot(3,1,1)
-        plt.plot(np.array(time_x), np.array(voltage_y))
-        plt.title("Voltage (V)")
-        plt.subplot(3,1,2)
-        plt.plot(np.array(time_x), np.array(current_y))
-        plt.title("Current (mA)")
-        plt.subplot(3,1,3)
-        plt.plot(np.array(time_x), np.array(power_y))
-        plt.title("Power (W)")
-        plt.xlabel("Time (s)")
-        plt.savefig("/home/pi/LCM-AutoBoat/testScripts/graph.png")
-        quit()
+    lc.handle()
+
 
