80515b8403f064a4a2a18a9610a4250a50627c8a
alexrice236 <alexrice826@gmail.com>
2022-07-29 19:52:02 -0400

    drop table

--- testScripts/data_collect.py
+++ testScripts/data_collect.py
@@ -9,6 +9,7 @@ power = "POW"
 
 power_db = "/home/pi/LCM-AutoBoat/testScripts/actuation_power.db"
 with sqlite3.connect(power_db) as c:
+    c.execute("""DROP TABLE actuation_power_data;""")
     c.execute("""CREATE TABLE IF NOT EXISTS actuation_power_data (source text, voltage decimal, current decimal, power decimal, time int);""")
 
 def my_handler(channel, data):
