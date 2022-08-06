e7785004f49f338dc1c0ae1549678ed7c3e8c4e9
alexrice236 <alexrice826@gmail.com>
2022-08-06 04:56:17 -0400

    40

--- testScripts/ui.py
+++ testScripts/ui.py
@@ -9,7 +9,7 @@ def press(key):
     msg.angle = 0.0
 
     if key == "w":
-        msg.linear_speed = 35
+        msg.linear_speed = 40
     if key == "a":
         msg.angle = 10
     if key == "s":
